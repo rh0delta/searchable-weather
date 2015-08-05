7257b22c08747c5a2fa87766f3e9c8be256d363a
Nayyir Jutha <nayyir.jutha@gmail.com>
2015-08-05 20:32:06 +0000

    called API and returned searchable cities

--- public/javascript/application.js
+++ public/javascript/application.js
@@ -1,4 +1,26 @@
-$(document).ready(function() {
+$(document).ready(function(){
+  var timerhandle;
+  var callAPI = function(url){
+    $.ajax({
+      dataType: "jsonp",
+      jsonp: "cb",
+      url: url
+    }).then(function(data){
+        console.log("API called");
+        $("#gen-cities").empty();
+        for(var i = 0; i < data.RESULTS.length; i++){
+          $("<a href='http://www.wunderground.com" + data.RESULTS[i].l + "'>" + data.RESULTS[i].name + "</a><br>").appendTo("#gen-cities");
+          console.log(data.RESULTS[i]);
+        }
+      });
+    }
+  $("#city").keyup(function(e){
+    e.preventDefault();
+    clearTimeout(timerhandle);
+    var query = $("#city").val();
+    var requestUrl = "http://autocomplete.wunderground.com/aq?query=" + query;
+    timerhandle = setTimeout(function(){callAPI(requestUrl)}, 1500);
+  });
 
-  // See: http://docs.jquery.com/Tutorials:Introducing_$(document).ready()
-});
+})
+  
