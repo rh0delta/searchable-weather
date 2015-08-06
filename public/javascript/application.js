764c36125be7874029fec757bb47c83365a7577f
Nayyir Jutha <nayyir.jutha@gmail.com>
2015-08-06 14:19:08 +0000

    iframe with webpage should show and hide based on clicked link also added some css styling

--- public/javascript/application.js
+++ public/javascript/application.js
@@ -3,9 +3,14 @@ function coolFunction(o1,o2){
     console.log(o1);
     console.log(o2);
     linkMe = "#" + o2;
-    $(linkMe).append("<iframe src='http://www.wunderground.com" + o1 + "'> \
+    
+    if ($("#weather").is(":visible")){
+      $(linkMe).empty();
+    } else {
+      $(linkMe).append("<iframe width='560' height='315' src='https://www.wunderground.com" + o1 + "' id='weather'> \
                 <p>Your browser does not support iFrames</p> \
               </iframe>");
+    }
   }
 $(document).ready(function(){
   var timerhandle;
@@ -20,10 +25,10 @@ $(document).ready(function(){
         console.log("API called");
         $("#gen-cities").empty();
         for(var i = 0; i < data.RESULTS.length; i++){
-          data.RESULTS[i].name = data.RESULTS[i].name.replace(/[^a-z0-9]/gi,'');
+          var nonspaced = data.RESULTS[i].name.replace(/[^a-z0-9]/gi,'');
           var linkStr = "<a href='#' onclick='javascript:coolFunction(";
-            linkStr += '"' + data.RESULTS[i].l + '","'+data.RESULTS[i].name+'"' +")'"+'>';
-            linkStr += data.RESULTS[i].name + "</a><br><div id='"+data.RESULTS[i].name+"'></div>";
+            linkStr += '"' + data.RESULTS[i].l + '","'+nonspaced+'"' +")'"+'>';
+            linkStr += data.RESULTS[i].name + "</a><br><div id='"+nonspaced+"'></div>";
           $(linkStr).appendTo("#gen-cities");
           console.log(linkStr);
           console.log(data.RESULTS[i]);
@@ -34,7 +39,7 @@ $(document).ready(function(){
     e.preventDefault();
     clearTimeout(timerhandle);
     var query = $("#city").val();
-    var requestUrl = "http://autocomplete.wunderground.com/aq?query=" + query;
+    var requestUrl = "https://autocomplete.wunderground.com/aq?query=" + query;
     timerhandle = setTimeout(function(){callAPI(requestUrl)}, 1500);
   });
 
